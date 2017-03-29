5ba96710b3aaeebd286bbde6e8aa7659d18668ba
Michael Savitski <tug28287@temple.edu>
2017-03-29 11:32:38 -0400

    Initial commit.

--- app/src/main/java/edu/temple/chatlab/MyFirebaseMessagingService.java
+++ app/src/main/java/edu/temple/chatlab/MyFirebaseMessagingService.java
@@ -4,11 +4,15 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.icu.text.SimpleDateFormat;
 import android.os.IBinder;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 import static android.content.ContentValues.TAG;
 
@@ -30,12 +34,25 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
 
+        Long millisecondTime = remoteMessage.getSentTime();
+
         Intent intent = new Intent("android.intent.action.MAIN")
-                .putExtra("incoming_message", remoteMessage.getNotification().getBody());
+                .putExtra("incoming_message", remoteMessage.getNotification().getBody() +
+                " - " + getDate(millisecondTime));
 
         this.sendBroadcast(intent);
         this.stopSelf();
 
+    }
+
+    public static String getDate(long milliSeconds)
+    {
+        Calendar cl = Calendar.getInstance();
+        cl.setTimeInMillis(milliSeconds);
+        String date = "" + cl.get(Calendar.MONTH) + "/" + cl.get(Calendar.DAY_OF_MONTH) + "/" + cl.get(Calendar.YEAR);
+        String time = "" + cl.get(Calendar.HOUR_OF_DAY) + ":" + cl.get(Calendar.MINUTE) + ":" + cl.get(Calendar.SECOND);
+        String dateAndTime = date + " " + time;
+        return dateAndTime;
     }
 
 }
